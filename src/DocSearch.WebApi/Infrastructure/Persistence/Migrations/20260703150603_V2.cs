1cb16f4d6bc69c50132632ae5f08beb3745516b7
amlsantos <6472330+santos-an@users.noreply.github.com>
2026-07-03 16:39:03 +0100

    new migration

---
+++ src/DocSearch.WebApi/Infrastructure/Persistence/Migrations/20260703150603_V2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocSearch.WebApi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedUtc",
+                table: "Documents",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastModifiedUtc",
+                table: "Documents");
+        }
+    }
+}
